65371b6b31e9a7e427634e07f3d1e7830b9b7a03
black-dev-x <thiago.dev.x@gmail.com>
2024-07-16 20:47:42 -0300

    update

--- book-list/src/App.vue
+++ book-list/src/App.vue
@@ -1,14 +1,26 @@
 <script setup>
 
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
+//ref is used for primitive values
 let count = ref(0)
+
+
+//reactive is used for objects (this is just a convention)
+let courses = [
+  { id: 1, title: 'Javascript' },
+  { id: 2, title: 'React' },
+  { id: 3, title: 'Vue' }
+]
 
 </script>
 
 <template>
   <div>
-    <h1>{{ count }}</h1>
+    <h1>Courses</h1>
+    <ul>
+      <li v-for="course in courses" :key="course.id">{{ course.title }}</li>
+    </ul>
     <button @click="count++">Increment</button>
   </div>
 </template>
