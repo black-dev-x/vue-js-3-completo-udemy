701cb0eb1fe694e91e5536c8440a9a07a90f0d83
black-dev-x <thiago.dev.x@gmail.com>
2024-07-29 18:39:17 -0300

    update

--- book-list/src/App.vue
+++ book-list/src/App.vue
@@ -3,7 +3,7 @@ const books = [
   {
     id: 1,
     title: 'History of Europe',
-    cover: 'https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg',
+    cover: 'https://whitegablespublishing.com/wp-content/uploads/2019/05/printpress-product-6-540x540.jpeg',
     isRead: true,
     isbn: '0-395-07157-8',
     author: 'Daniel Trejo',
@@ -11,7 +11,7 @@ const books = [
   {
     id: 2,
     title: 'Penguin Classics',
-    cover: 'https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-2-540x861.jpg',
+    cover: 'https://whitegablespublishing.com/wp-content/uploads/2019/05/printpress-product-2-540x540.jpeg',
     isRead: false,
     isbn: '0-395-07157-8',
     author: 'Daniel Trejo, Jon Snow',
@@ -19,7 +19,7 @@ const books = [
   {
     id: 3,
     title: 'Becoming',
-    cover: 'https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-7-540x861.jpg',
+    cover: 'https://whitegablespublishing.com/wp-content/uploads/2019/05/printpress-product-7-540x540.jpeg',
     isRead: false,
     isbn: '0-395-07157-8',
     author: 'Daniel Trejo',
@@ -27,7 +27,7 @@ const books = [
   {
     id: 4,
     title: 'Sonnets',
-    cover: 'https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-5-540x861.jpg',
+    cover: 'https://whitegablespublishing.com/wp-content/uploads/2019/05/printpress-product-5-540x540.jpeg',
     isRead: false,
     isbn: '0-395-07157-8',
     author: 'Daniel Trejo',
@@ -44,19 +44,18 @@ const books = [
 
     <div class="books-container">
       <div class="books-list">
-        <div class="book">
+        <div v-for="book in books" :key="book.id" class="book">
           <div class="book-cover">
-            <img src="https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg" />
-
+            <img :src="book.cover" />
             <button>
               <i class="fa-solid fa-eye"></i>
               <span>Ainda não li</span>
             </button>
           </div>
           <div class="book-details">
-            <p class="book-author">Daniel Trejo</p>
-            <h3 class="book-title">History of Europe</h3>
-            <p><i class="fa-solid fa-hashtag icon"></i> 0-395-07157-8</p>
+            <p class="book-author">{{ book.author }}</p>
+            <h3 class="book-title">{{ book.title }}</h3>
+            <p><i class="fa-solid fa-hashtag icon"></i>{{ book.isbn }}</p>
           </div>
         </div>
       </div>
