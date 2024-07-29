c8be40cc87f052eeb468fbbadfc68620463b329a
black-dev-x <thiago.dev.x@gmail.com>
2024-07-28 22:56:17 -0300

    update

--- book-list/src/App.vue
+++ book-list/src/App.vue
@@ -1,18 +1,20 @@
 <script setup>
 
 import { ref, reactive } from 'vue'
-
+import Progress from './components/Progress.vue'
 //reactive is used for objects (this is just a convention)
 let id = 4
-let courses = [
-  { id: 1, title: 'Javascript' },
-  { id: 2, title: 'React' },
-  { id: 3, title: 'Vue' }
-]
+let courses = reactive([
+  { id: 1, title: 'Javascript', done: true },
+  { id: 2, title: 'React', done: false },
+  { id: 3, title: 'Vue', done: false }
+])
+
 const addCourse = () => {
-  courses.push({ id, title: courseName.value })
+  courses.push({ id: ++id, title: courseName.value, done: false })
   courseName.value = ''
 }
+
 let courseName = ref('')
 
 </script>
@@ -27,6 +29,7 @@ let courseName = ref('')
   <br>
   <input type="text" v-model="courseName">
   <button @click="addCourse">Add Course</button>
+  <Progress :courses="courses"/>
 </template>
 
 <style scoped>
---
+++ book-list/src/components/Progress.vue
@@ -0,0 +1,10 @@
+<script setup>
+  const props = defineProps(['courses'])
+</script>
+
+<template>
+  <div>
+    {{courses.length}}
+  </div>
+  <br>
+</template>
