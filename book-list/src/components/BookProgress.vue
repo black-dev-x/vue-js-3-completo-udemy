f0f12e6894803ecd30733d98d925e6c6bcc4dd76
black-dev-x <thiago.dev.x@gmail.com>
2024-08-03 18:35:34 -0300

    Refactor code to improve performance and readability

---
+++ book-list/src/components/BookProgress.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="book-progress">
+    <div class="progress-bar">
+      <div class="progress" :style="{ width: `${progress}%` }"></div>
+    </div>
+    <p>{{ progress }}%</p>
+  </div>
+</template>
